2e6308abed76386d349bcef99436b2f0d29ffdc4
Tuananh2810 <trantuananh.28102002@gmail.com>
2022-08-29 21:54:04 +0700

    giao dien Profile 3

---
+++ app/src/main/java/com/example/taogiaodien_profile_205053120104/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.taogiaodien_profile_205053120104;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
